dc3ce54676b73b1168f555ab751fc9ca4120389a
Konrad Baechler <konrad@diva.exchange>
2020-05-26 17:32:40 +0200

    entrypoint fixes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Hyperledger/Iroha Blockchain
 
-The blockchain backend of _diva_ is based on Hyperledger/Iroha. See here: https://iroha.readthedocs.io/en/latest/index.html 
+The blockchain backend of _diva_ is based on Hyperledger/Iroha. See here: https://iroha.readthedocs.io/en/master/
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,30 +4,13 @@
 #
 set -e
 
-# optional
-IDENT_INSTANCE=${IDENT_INSTANCE:-0}
-
-# derived
-# LOCAL_IROHA_NODE_KEY=holodeck${IDENT_INSTANCE:?err}
-
-# @TODO improve isolation? iptables not possible within docker?
-# isolated networking
+# networking
 echo "nameserver 127.0.1.1" >/etc/resolv.conf
 cat </dnsmasq.conf >/etc/dnsmasq.conf
 dnsmasq -a 127.0.1.1 \
   --no-hosts \
   --local-service \
   --address=/#/127.0.0.1
-
-# proxying iroha traffic
-#su node -c "cd /opt/iroha/ && node -r esm ./app/proxy-out.js &"
-
-# echo key: ${LOCAL_IROHA_NODE_KEY}
-
-# remove unused keys
-# mv -f ${LOCAL_IROHA_NODE_KEY}* ../
-# rm -f holodeck*
-# mv -f ../${LOCAL_IROHA_NODE_KEY}* ./
 
 #postgres configuration
 cat </postgresql.conf >/etc/postgresql/10/main/postgresql.conf
@@ -36,21 +19,26 @@ service postgresql start
 /wait-for-it.sh localhost:5432 -t 30 -s --
 
 # set the postgres password to 'iroha'
-# this should be NO security issue, because
+# this shouldn't be a security issue, because
 # - the access to the database is from within the container only
 # - all data on the database is public and only a mirror (to increase efficiency and performance) of the blockchain
 su postgres -c "psql -c \"ALTER USER postgres PASSWORD 'iroha';\""
 
+# create the databases
+su postgres -c "createdb iroha_data0 && createdb iroha_data1 && createdb iroha_data2"
+
 # start iroha
-nohup /usr/bin/irohad --config config0.json --keypair_name holodeck0 &
-nohup /usr/bin/irohad --config config1.json --keypair_name holodeck1 &
-nohup /usr/bin/irohad --config config2.json --keypair_name holodeck2 &
+/usr/bin/irohad --config config0.json --keypair_name holodeck0 2>&1 &
+sleep 5
+/usr/bin/irohad --config config1.json --keypair_name holodeck1 2>&1 &
+sleep 5
+/usr/bin/irohad --config config2.json --keypair_name holodeck2 2>&1 &
 
 while sleep 60; do
-  ps aux |grep irohad |grep -q -v grep
-  PROCESS_1_STATUS=$?
+  ps aux | grep irohad | grep -q -v grep
+  PROCESS_STATUS=$?
   # If the greps above find anything, they exit with 0 status
-  if [ $PROCESS_1_STATUS -ne 0 ]; then
+  if [ $PROCESS_STATUS -ne 0 ]; then
     echo "One of the processes has already exited."
     exit 1
   fi

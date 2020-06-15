44f24185c79b1aad64a1eb6f5e53cb34271b6789
Konrad Baechler <konrad@diva.exchange>
2020-06-15 07:48:46 +0200

    compatibility with Windows: -v instead --mount

--- bin/run.sh
+++ bin/run.sh
@@ -9,4 +9,4 @@ cd ${PROJECT_PATH}/../
 docker volume create iroha
 
 # start the container
-docker run -d -p 25432:5432 -p 50151:50051 --name=iroha --mount type=volume,src=iroha,dst=/opt/iroha/data/ divax/iroha:latest
+docker run -d -p 25432:5432 -p 50151:50051 --name=iroha -v iroha:/opt/iroha/data divax/iroha:latest
